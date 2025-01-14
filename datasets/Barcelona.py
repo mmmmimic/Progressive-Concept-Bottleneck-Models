71206d7190e3fd12cce5c999faa815bbc76fe712
Manxi Lin <46046604+mmmmimic@users.noreply.github.com>
2025-01-14 13:17:31 +0100

    Update Barcelona.py

--- datasets/Barcelona.py
+++ datasets/Barcelona.py
@@ -3,7 +3,6 @@ Fetal data, 3rd trim
 '''
 import os
 import numpy as np
-from matplotlib import pyplot as plt
 import torch
 from torch.utils.data import Dataset, DataLoader
 import pandas as pd
@@ -184,7 +183,6 @@ class BarcelonaHead(Dataset):
         return self.labels
 
 if __name__ == "__main__":
-
     traindata = BarcelonaHead('train')
     print(len(traindata))
     traindata = BarcelonaHead('test')
@@ -192,9 +190,3 @@ if __name__ == "__main__":
     loader = DataLoader(traindata, batch_size=32, drop_last=False, shuffle=True)
     from collections import Counter
     print(Counter(traindata.labels))
-    plt.figure()
-    for image, label in tqdm(loader):
-        # print(image.shape)
-        print(label)
-        plt.imshow((image[10,...].squeeze().detach().cpu().numpy()+1)/2)
-        plt.show()
